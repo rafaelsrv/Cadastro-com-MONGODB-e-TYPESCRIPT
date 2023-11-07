689bb0d5df23a1e522c4a43da590bb0bbf325bd0
RafaelSRV <rafael-srv@hotmail.com>
2023-11-07 13:54:02 -0300

    Cadastrando e listando clientes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,46 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useRef, FormEvent} from 'react'
 import { FiTrash } from 'react-icons/fi'
 import { api } from './services/api'
+
+interface CustomerProps{
+  id: string;
+  name: string;
+  email: string;
+  status: boolean;
+  created_at: string;
+}
 
 
 
 export default function App(){
 
-  useEffect(()=>{
+  const [customers, setCustomers] = useState<CustomerProps[]>([])
+  const nameRef = useRef<HTMLInputElement | null >(null)
+  const emailRef = useRef<HTMLInputElement | null >(null)
 
+  useEffect(()=>{
+    loadCustomers();
 
   }, [])
 
   async function loadCustomers(){
     const response = await api.get("/customers")
-    console.log(response)
+    setCustomers(response.data);
+  }
+
+  async function handleSubmit(event: FormEvent){
+    event.preventDefault();
+
+    if(!nameRef.current?.value || !emailRef.current?.value) return;
+
+    const response = await api.post("/customer",{
+      name: nameRef.current?.value,
+      email: emailRef.current?.value
+    })
+
+    setCustomers(allCustomers =>[...allCustomers, response.data] )
+    console.log(setCustomers)
+
   }
 
   return(
@@ -22,35 +49,40 @@ export default function App(){
       <main className="my-10 w-full md:max-w-2xl">
         <h1 className="text-4x1 font-medium text-white">Clientes</h1>
 
-        <form className="flex flex-col my-6">
+        <form className="flex flex-col my-6" onSubmit={handleSubmit}>
           <label className="font-medium text-white">Nome:</label>
           <input 
           type="text"
           placeholder="Digite seu nome completo..."
-          className="w-full mb-5 p-2 rounded" />
+          className="w-full mb-5 p-2 rounded" 
+          ref={nameRef}/>
 
           <label className="font-medium text-white">Email:</label>
           <input 
           type="text"
           placeholder="Digite seu Email..."
-          className="w-full mb-5 p-2 rounded" />
+          className="w-full mb-5 p-2 rounded" 
+          ref={emailRef}/>
 
           <input type="submit" value="Cadastrar" className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" />
         </form>
 
           <section className='flex flex-col'>
 
-            <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+            {customers.map((customer)=>(
               
-              <p>  <span className="font-medium">Nome:</span> Rafael </p>
-              <p>  <span className="font-medium">Email:</span> teste@teste.com </p>
-              <p>  <span className="font-medium">Status:</span> ATIVO </p>
+              <article key={customer.id} className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+              
+              <p>  <span className="font-medium">Nome:</span> {customer.name} </p>
+              <p>  <span className="font-medium">Email:</span> {customer.email} </p>
+              <p>  <span className="font-medium">Status:</span> {customer.status ? "ATIVO" : "INATIVO"} </p>
               
               <button className='bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2 '>
                 <FiTrash size={18} color="#FFF"/>
               </button>
 
             </article>
+            ))}
           </section>
 
 
