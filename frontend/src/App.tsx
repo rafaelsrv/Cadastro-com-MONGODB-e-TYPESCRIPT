f5add78974a33fd810f7ff737e666053a7f1c220
RafaelSRV <rafael-srv@hotmail.com>
2023-11-07 13:07:57 -0300

    implementando form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from 'react'
+import { FiTrash } from 'react-icons/fi'
+import { api } from './services/api'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+export default function App(){
+
+  useEffect(()=>{
+
+
+  }, [])
+
+  async function loadCustomers(){
+    const response = await api.get("/customers")
+    console.log(response)
+  }
+
+  return(
+    <div>
+     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4x1 font-medium text-white">Clientes</h1>
+
+        <form className="flex flex-col my-6">
+          <label className="font-medium text-white">Nome:</label>
+          <input 
+          type="text"
+          placeholder="Digite seu nome completo..."
+          className="w-full mb-5 p-2 rounded" />
+
+          <label className="font-medium text-white">Email:</label>
+          <input 
+          type="text"
+          placeholder="Digite seu Email..."
+          className="w-full mb-5 p-2 rounded" />
+
+          <input type="submit" value="Cadastrar" className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" />
+        </form>
+
+          <section className='flex flex-col'>
+
+            <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+              
+              <p>  <span className="font-medium">Nome:</span> Rafael </p>
+              <p>  <span className="font-medium">Email:</span> teste@teste.com </p>
+              <p>  <span className="font-medium">Status:</span> ATIVO </p>
+              
+              <button className='bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2 '>
+                <FiTrash size={18} color="#FFF"/>
+              </button>
+
+            </article>
+          </section>
+
+
+        
+      </main>
+     </div>
+    </div>
   )
 }
-
-export default App
