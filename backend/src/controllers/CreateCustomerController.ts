b2ad2f872f5980fa7e3602a48493b891825a0322
RafaelSRV <rafael-srv@hotmail.com>
2023-11-06 21:14:08 -0300

    Fazendo posts no banco - MongoDB

--- backend/src/controllers/CreateCustomerController.ts
+++ backend/src/controllers/CreateCustomerController.ts
@@ -3,10 +3,12 @@ import { CreateCustomerService } from "../services/CreateCustomerService";
 
 class CreateCustomerController{
     async handle(request: FastifyRequest, reply: FastifyReply ){
+        const { name, email } = request.body as { name: string, email: string }
+        
         
         const customerService = new CreateCustomerService()
 
-        const customer = await customerService.execute();
+        const customer = await customerService.execute({name, email});
 
         reply.send(customer)
     }
